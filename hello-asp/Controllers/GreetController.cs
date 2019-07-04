20f98eb86081e986d055cadbcac684b236882761
ks-developer <ksapp@aok-net.com>
2019-07-04 15:18:15 +0900

    プロジェクト ファイルを追加します。

---
+++ hello-asp/Controllers/GreetController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hello_asp.Controllers
+{
+    public class GreetController : Controller
+    {
+        public IActionResult Index(string name)
+        {
+            ViewData["name"] = name;
+            return View();
+        }
+    }
+}
---
+++ hello-asp/Controllers/HelloController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using hello_asp.Models;
+
+namespace hello_asp.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            HelloModel model = new HelloModel();
+            model.Name = "名無しさん";
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(HelloModel helloModel)
+        {
+            return View(helloModel);
+        }
+    }
+}
