a14e7b9fc9be294612ff331c80f4c38c93548fc6
ksdueki <ueki@aok-net.com>
2019-07-04 15:20:38 +0900

    fix

--- hello-asp/Controllers/HelloController.cs
+++ hello-asp/Controllers/HelloController.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using hello_asp.Models;
 using Microsoft.AspNetCore.Mvc;
-
-using hello_asp.Models;
 
 namespace hello_asp.Controllers
 {
